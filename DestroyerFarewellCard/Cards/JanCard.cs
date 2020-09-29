233d2170dacd4c3e740c24adc511affbee52421c
Jan Marais <jan.marais@eurmonitor.com>
2020-09-29 15:15:17 +0200

    updated

--- DestroyerFarewellCard/Cards/JanCard.cs
+++ DestroyerFarewellCard/Cards/JanCard.cs
@@ -1,13 +1,19 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
 
 namespace DestroyerFarewellCard.Cards
 {
     public class JanCard : AbstractCard
     {
-        private static readonly string MESSAGE = "*****************************\nHey Lukas, I'm really going to miss you in the work place dude. \nIt won't be same and I'm definitely going to miss the 'Sure Jan' gifs alot, but once this covid nonsense is over I'll see you in the new building :). \nAll the best for the new work endeavours. It's been an honour to work with you. /\nHappy destruction Destroyer.\n*****************************";
-
-        private static readonly string IMAGE_URI = "./Images/Metallica.png"; //Put image in Images folder and make sure 
-                                                                              //its build properties are set to copy to output
+        private static readonly string MESSAGE = "*****************************" +
+            "\nHey Lukas, gonna miss you in the work place dude." +
+            "\nIt won't be same and definitely going to miss the 'Sure Jan' gifs alot :)." +
+            "\nAll the best for the new work endeavours. It's been an honour to work with you." +
+            "\nHappy Destruction Destroyer." +
+            "\n*****************************";
+        private static readonly string IMAGE_URI = "./Images/Metallica.png";
 
         public override string GetCardMessage()
         {
