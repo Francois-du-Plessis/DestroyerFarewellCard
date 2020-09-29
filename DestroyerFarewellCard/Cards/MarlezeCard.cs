43bd9281b7207c7da99cf93bb7e57996ca036788
Francois Du Plessis <Francois.DuPlessis@Euromonitor.com>
2020-09-29 18:00:01 +0200

    Added Marleze's card

---
+++ DestroyerFarewellCard/Cards/MarlezeCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace DestroyerFarewellCard.Cards
+{
+    public class MarlezeCard : AbstractCard
+    {
+        private static readonly string MESSAGE = "======================================================================================================================\n" +
+            "\tWishing you all the best with your new role.\n" +
+            "\tLook forward to you popping in for a cuppa and to say hi.\n" +
+            "======================================================================================================================";
+        
+        private static readonly string IMAGE_URI = ""; //Put image in Images folder and make sure 
+                                                       //its build properties are set to copy to output
+
+        public override string GetCardMessage()
+        {
+            return MESSAGE;
+        }
+
+        public override Bitmap GetImage()
+        {
+            return GetBitmapFromUri(IMAGE_URI);
+        }
+    }
+}
