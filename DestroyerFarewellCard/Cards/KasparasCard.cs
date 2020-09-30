33241dd204a499997f5d29bf84abcc15d6d4cbf9
Francois Du Plessis <Francois.DuPlessis@Euromonitor.com>
2020-09-30 14:30:21 +0200

    Added KasparasCard

---
+++ DestroyerFarewellCard/Cards/KasparasCard.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace DestroyerFarewellCard.Cards
+{
+    public class KasparasCard : AbstractCard
+    {
+        private static readonly string MESSAGE = "It was pleasure working with you. We haven't work for long, but it was nice. Best of Luck in your next job.";
+        private static readonly string IMAGE_URI = "./Images/GoodLuck.jpg";
+        public override string GetCardMessage()
+        {
+            return MESSAGE;
+        }
+        public override Bitmap GetImage()
+        {
+            return GetBitmapFromUri(IMAGE_URI);
+        }
+        public override Order GetOrder()
+        {
+            return Order.ImageFirst;
+        }
+    }
+}
