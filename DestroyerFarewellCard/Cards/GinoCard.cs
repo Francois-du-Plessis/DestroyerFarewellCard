074daf8d9c0b63d8f353e7a39589b170f7b77e4a
Francois Du Plessis <Francois.DuPlessis@Euromonitor.com>
2020-09-30 09:59:19 +0200

    Added Gino's message

---
+++ DestroyerFarewellCard/Cards/GinoCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace DestroyerFarewellCard.Cards
+{
+    public class GinoCard : AbstractCard
+    {
+        private static readonly string MESSAGE = "======================================================================================================================\n" +
+            "\tLukas - Wishing you only the best on your new adventure. \n" +
+            "\tMay the “Rekt” gods shine upon The Destroyer with great favour and fortune :D\n" +
+            "======================================================================================================================";
+        
+        private static readonly string IMAGE_URI = ""; //Put image in Images folder and make sure 
+                                                       //its build properties are set to copy to output
+
+        public override string GetCardMessage()
+        {
+            return MESSAGE;
+        }
+
+        public override Bitmap GetImage()
+        {
+            return GetBitmapFromUri(IMAGE_URI);
+        }
+    }
+}
